c61431726bfed9b111482aacf47351541b139a6b
Jiangshan00001 <710806594@qq.com>
2017-07-16 16:08:36 +0800

    add a button in issue view form. function of this button is to mark all notification of this issue as viewed. and redirect to notification page.
    add a button in issue view form. function of this button is to mark all notification of this issue as viewed. and redirect to notification page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 get 'app-notifications', :to => 'app_notifications#index'
 get 'app-notifications/:id', :to => 'app_notifications#view'
 post 'app-notifications/view-all', :to => 'app_notifications#view_all'
+post 'app-notifications/view_by_issue_id', :to => 'app_notifications#view_by_issue_id'
