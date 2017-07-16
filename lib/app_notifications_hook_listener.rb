b0c8932d185c5480c98112b81d0a1a7947c13d1d
Jiangshan00001 <710806594@qq.com>
2017-07-16 16:09:08 +0800

    add a button in issue view form. function of this button is to mark all notification of this issue as viewed. and redirect to notification page.
    add a button in issue view form. function of this button is to mark all notification of this issue as viewed. and redirect to notification page.

--- lib/app_notifications_hook_listener.rb
+++ lib/app_notifications_hook_listener.rb
@@ -1,4 +1,5 @@
 class AppNotificationsHookListener < Redmine::Hook::ViewListener
   render_on :view_my_account_preferences, :partial => "app_notifications/my_account_preferences"
   render_on :view_layouts_base_html_head, :partial => "app_notifications/layouts_base_html_head"
+  render_on(:view_issues_show_details_bottom, :partial => "app_notifications/mark_notify_by_issue"  )
 end
